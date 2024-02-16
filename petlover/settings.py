97209b971b442ad2f23b010d79dd9f0c71ed5bb6
naisepaul <>
2024-02-16 16:11:23 +0000

    set debug to false due to error after deployment

--- petlover/settings.py
+++ petlover/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR =  os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['8000-naisepaul-petlover-proje-ei541ie28e.us2.codeanyapp.com']
 ALLOWED_HOSTS = ['petlover-6900a2845617.herokuapp/', os.environ.get("LOCAL_HOST")]
