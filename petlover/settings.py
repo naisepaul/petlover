6b8e714a95bb32ee8df9505d14c0e2b61bb677c3
naisepaul <>
2024-02-16 16:05:56 +0000

    changed the allowed host details

--- petlover/settings.py
+++ petlover/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = ['8000-naisepaul-petlover-proje-ei541ie28e.us2.codeanyapp.com']
-ALLOWED_HOSTS = ['petlover.herokuapp', 'localhost']
+ALLOWED_HOSTS = ['petlover-6900a2845617.herokuapp', 'localhost']
 
 
 # Application definition
