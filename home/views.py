31b65f3a7b6d272f1d1756c62792e5b9968a8dba
Naise Paul <naisepaul@gmail.com>
2024-02-25 00:51:58 +0000

    Added base.html and index.html

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse('hello world')
+def index(request):
+    return render(request,'index.html')
