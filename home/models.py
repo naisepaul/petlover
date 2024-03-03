ce9163b2b8425a47dbfbbc0c89808d259f0d6cd8
Naise Paul <naisepaul@gmail.com>
2024-03-03 16:54:34 +0000

    Connected user signup form to server

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # from django_resized import ResizedImageField
 from cloudinary.models import CloudinaryField
-import uuid
+# import uuid1
 
 # Create your models here.
 
@@ -12,14 +12,14 @@ class Dog(models.Model):
         ('F', 'Female'),     
     ]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    breed = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    sex = models.CharField(max_length=1, choices=SEX_CHOICES, default='M')
-    description = models.TextField(null=True, blank=True)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # breed = models.CharField(max_length=100)
+    # # date_of_birth = models.DateField()
+    # sex = models.CharField(max_length=1, choices=SEX_CHOICES, default='M')
+    # description = models.TextField(null=True, blank=True)
 
-    def __str__(self):
-        return self.breed
+    # def __str__(self):
+    #     return self.breed
 
 # class Listing(models.Model):
 #     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -9,6 +10,16 @@ def listings(request):
     return render(request,'listings.html')
 
 def signup(request):
+    if request.method == "POST":
+        name = request.POST.get('fname')   
+        uname = request.POST.get('username')      
+        email = request.POST.get('email')        
+        password = request.POST.get('password1')
+        confirmpassword = request.POST.get('password2')
+        
+        myuser = User.objects.create_user(uname, email, password)
+        myuser.save()
+        return HttpResponse("Signup Succesfull")
     return render(request,'account/signup.html')
     
 def login(request):
