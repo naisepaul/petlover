d1b2b569012b6436a4951559de8cb23a95eb9e76
Naise Paul <naisepaul@gmail.com>
2024-03-14 00:03:18 +0000

    Added a function to check the username and email id is already exist in the database

--- home/forms.py
+++ home/forms.py
@@ -3,22 +3,21 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
-# Class UserUpdateForm(forms.ModelForm):
-#     email = forms.EmailField
-
-#     Class Meta:
-#         Model = User
-#         fields =['name', 'username', 'email']
-
-# Class ProfileUpdateForm(forms.ModelForm):
-#     email = forms.EmailField
-
-#     Class Meta:
-#         Model = User
-#         fields =['phone', 'profile_image']    
-
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_image','name', 'username', 'email','phone']  # Adjust fields as per your UserProfile model
 
+
+    def clean(self):
+            cleaned_data = super().clean()
+            username = cleaned_data.get("username")
+            email = cleaned_data.get("email")
+            
+            # Check if the entered username already exists
+            if User.objects.exclude(pk=self.instance.user_id).filter(username=username).exists():
+                self.add_error('username', 'This username is already in use.')
+            
+            # Check if the entered email already exists
+            if User.objects.exclude(pk=self.instance.user_id).filter(email=email).exists():
+                self.add_error('email', 'This email address is already in use.')
