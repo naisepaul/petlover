12abf1b05744c4444c2fe8ddf9c59a1743dd6166
Naise Paul <naisepaul@gmail.com>
2024-03-14 00:20:51 +0000

    Returned cleaned data if email and username exists and revert back to old details

--- home/forms.py
+++ home/forms.py
@@ -17,7 +17,10 @@ class ProfileForm(forms.ModelForm):
             # Check if the entered username already exists
             if User.objects.exclude(pk=self.instance.user_id).filter(username=username).exists():
                 self.add_error('username', 'This username is already in use.')
+                cleaned_data['username'] = self.instance.user.username
             
             # Check if the entered email already exists
             if User.objects.exclude(pk=self.instance.user_id).filter(email=email).exists():
                 self.add_error('email', 'This email address is already in use.')
+                cleaned_data['email'] = self.instance.user.email
+            return cleaned_data    
